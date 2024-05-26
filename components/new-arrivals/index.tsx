0ec041660146eb9105d8acd5e69476383a8180f3
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-05-26 23:54:28 +0100

    chore: Improve New Arrivals component image rendering and styles

--- components/new-arrivals/index.tsx
+++ components/new-arrivals/index.tsx
@@ -8,11 +8,14 @@ const NewArrivals = async () => {
   return (
     <div className="mt-20 flex h-screen flex-col items-center gap-8 p-10">
       <h2 className="text-2xl font-semibold uppercase">New Arrivals</h2>
-      <div className="flex  gap-7  *:h-80 *:w-52  *:border *:border-slate-700  ">
+      <div className="flex  gap-7  *:h-80 *:w-52 ">
         {latestProducts.map((product) => (
-          <div className="flex  flex-col items-start justify-between gap-4 " key={product.id}>
+          <div
+            className="flex flex-col   items-start justify-between gap-4 hover:cursor-pointer "
+            key={product.id}
+          >
             <Image
-              className="h-full"
+              className="h-full transform transition-transform duration-300 hover:scale-105"
               src={product.featuredImage?.url}
               height={400}
               width={200}
