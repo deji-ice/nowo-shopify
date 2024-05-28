2e3ef42aa7b9a06ef5ead4b357b70041a5d7296e
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-05-28 17:35:45 +0100

    chore: Update footer component with additional links and styles

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -15,8 +15,8 @@ export default async function Footer() {
   const copyrightName = COMPANY_NAME || SITE_NAME || '';
 
   return (
-    <footer className="text-sm text-neutral-500 dark:text-neutral-400">
-      <div className="mx-auto flex w-full max-w-7xl flex-col gap-6 border-t border-neutral-200 px-6 py-12 text-sm md:flex-row md:gap-12 md:px-4 min-[1320px]:px-0 dark:border-neutral-700">
+    <footer className="bg-black px-20 text-sm text-neutral-500 dark:text-neutral-400">
+      <div className="mx-auto flex w-full max-w-7xl flex-col justify-between gap-6 border-t border-neutral-200 px-6 py-12 text-sm md:flex-row md:gap-12 md:px-4 min-[1320px]:px-0 dark:border-neutral-700">
         <div>
           <Link className="flex items-center gap-2 text-black md:pt-1 dark:text-white" href="/">
             <LogoSquare size="sm" />
@@ -37,6 +37,35 @@ export default async function Footer() {
         >
           <FooterMenu menu={menu} />
         </Suspense>
+        <div className="flex gap-28 text-white">
+          <div className="flex gap-16">
+            <ul className="flex flex-col gap-1">
+              <li className="mb-2 uppercase">
+                <h3>Category</h3>
+              </li>
+              <li>Women</li>
+              <li>About</li>
+              <li>Home</li>
+            </ul>
+            <ul className="flex flex-col gap-1">
+              <li className="mb-2 uppercase">
+                <h3>Collections</h3>
+              </li>
+              <li>Info</li>
+              <li>About</li>
+              <li>Home</li>
+            </ul>
+            <ul className="flex flex-col gap-1">
+              <li className="mb-2 uppercase">
+                <h3>Contacts</h3>
+              </li>
+              <li>Info about contacts</li>
+            </ul>
+          </div>
+          <div className="flex flex-col  gap-1 ">
+            <h3 className="mb-2  uppercase">Social Media</h3> <span>logos</span>
+          </div>
+        </div>
         {/* <div className="md:ml-auto">
           <a
             className="flex h-8 w-max flex-none items-center justify-center rounded-md border border-neutral-200 bg-white text-xs text-black dark:border-neutral-700 dark:bg-black dark:text-white"
@@ -53,7 +82,8 @@ export default async function Footer() {
         <div className="mx-auto flex w-full max-w-7xl flex-col items-center gap-1 px-4 md:flex-row md:gap-0 md:px-4 min-[1320px]:px-0">
           <p>
             &copy; {copyrightDate} {copyrightName}
-            {copyrightName.length && !copyrightName.endsWith('.') ? '.' : ''} All rights reserved.
+            {copyrightName.length && !copyrightName.endsWith('.') ? '.' : ''}
+            All rights reserved.
           </p>
           <hr className="mx-4 hidden h-4 w-[1px] border-l border-neutral-400 md:inline-block" />
           <p>Designed in Nigeria</p>
