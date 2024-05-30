6502ad7bbce26dd6d9fd865efe42da442b1a7f44
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-05-30 09:37:40 +0100

    chore: Improve New Arrivals component image rendering and styles

--- components/new-arrivals/index.tsx
+++ components/new-arrivals/index.tsx
@@ -1,3 +1,4 @@
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import { getProducts } from 'lib/shopify';
 import Image from 'next/image';
 import React from 'react';
@@ -7,7 +8,12 @@ const NewArrivals = async () => {
   const latestProducts = products.slice(0, 5); // Get the latest 5 products
   return (
     <div className="mt-20 flex h-screen flex-col items-center gap-8 p-10">
-      <h2 className="text-2xl font-semibold uppercase">New Arrivals</h2>
+      <div className="flex w-full items-center justify-between px-10">
+        <h2 className="text-xl font-semibold uppercase">New Arrivals</h2>
+        <span className=" flex items-center ">
+          MORE <ArrowRightIcon className="h-4" />
+        </span>
+      </div>
       <div className="flex  gap-7  *:h-80 *:w-52 ">
         {latestProducts.map((product) => (
           <div
@@ -21,9 +27,9 @@ const NewArrivals = async () => {
               width={200}
               alt={product.description}
             />
-            <div className="flex w-full items-center justify-between px-2 text-sm">
-              <p>{product.title}</p>{' '}
-              <p className="font-semibold">₦{product.priceRange.maxVariantPrice.amount}</p>
+            <div className="flex w-full flex-col items-center justify-start px-2 text-sm">
+              <p className="font-semibold">{product.title}</p>
+              <p className="font-light">₦{product.priceRange.maxVariantPrice.amount}</p>
             </div>
           </div>
         ))}
